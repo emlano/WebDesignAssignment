33069d2c5a697e065b173b891c0b9b63eaf5ee98
Emil Milano <82947413+aemilano@users.noreply.github.com>
2023-07-15 16:32:23 +0530

    Improve cart animations/simplify JS

--- src/shop/script.js
+++ src/shop/script.js
@@ -1,4 +1,15 @@
-function dropCart() {
-    console.log("Hello World");
-    document.getElementById("cart").classList.toggle("show-cart");
-}
+ const cart = document.getElementById("cart");
+ const cartButton = document.getElementById("cart-button");
+
+cartButton.addEventListener('click', (event) => {
+    cart.classList.toggle("show-cart");
+    event.stopPropagation();
+})
+
+document.addEventListener('click', (event) => {
+    if (event.target.closest('#cart')) {
+        return;
+    }
+
+    cart.classList.remove('show-cart');
+})
