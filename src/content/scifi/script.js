3486037962a75e20ec396bf40bbfb4d41d1cd466
AbrarSL <abrarsl2002@gmail.com>
2023-07-24 00:37:52 +0530

    Scroll to the very top of the page with JS

--- src/content/scifi/script.js
+++ src/content/scifi/script.js
@@ -1,5 +1,6 @@
 import { showDialog } from "../../common.js";
 
+// Add an image dialog to all the images inside a .movie-info card
 document.querySelectorAll(".movie-info > img").forEach(img => {
   img.addEventListener("click", event => {
     const imgElement = document.createElement("img");
@@ -10,3 +11,12 @@ document.querySelectorAll(".movie-info > img").forEach(img => {
     showDialog(title, "", [imgElement]);
   });
 });
+
+// Scroll to the top
+document
+  .querySelector("#page-top-link")
+  .addEventListener("click", () => document.body.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "auto",
+  }));
